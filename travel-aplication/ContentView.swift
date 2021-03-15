0b63161bb7cda99d46364eed2e943d30a01a25a3
Ronaldo Vargas <ronaldovh99@gmail.com>
2021-03-15 18:35:41 -0500

    Initial Commit

---
+++ travel-aplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  travel-aplication
+//
+//  Created by Ronaldo Andre Vargas Huaman on 15/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ travel-aplication/travel_aplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_aplicationApp.swift
+//  travel-aplication
+//
+//  Created by Ronaldo Andre Vargas Huaman on 15/03/21.
+//
+
+import SwiftUI
+
+@main
+struct travel_aplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
